375d8dcdbc6ca3d0826391b660c6d50214c6534e
Nova <hometny@gmail.com>
2015-04-05 11:49:14 +0300

    [fix]

---
+++ application/views/topmenu.php
@@ -0,0 +1,10 @@
+<div class="topMenu">
+    <ul>
+        <li>
+            <img src="application/img/logo.png">
+        </li>
+        <li>
+            <div style="float: right;" class="bottom"><a href="">ВОЙТИ В СИСТЕМУ</a></div>
+        </li>
+    </ul>
+</div>
